256fbd366dbf5eff94b49a3f3841b366ed604514
Yuri Elva <eyuri6359@gmail.com>
2026-05-26 22:46:14 +0700

    Final Code

--- Smart_City/iet_2026_24782097/usermanagement_24782097/urls.py
+++ Smart_City/iet_2026_24782097/usermanagement_24782097/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import UserLoginView, UserLogoutView, register
+from .api_views import RegisterView
+
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+
+    # API Register
+    path('api/register/', RegisterView.as_view(), name='api_register'),
 ]
