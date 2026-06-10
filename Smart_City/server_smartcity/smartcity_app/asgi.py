1d6c4ebddae30c203a257d94fb64e8cad45a970d
Yuri Elva <eyuri6359@gmail.com>
2026-06-10 14:36:20 +0700

    LabSession13 - Tahap 1: Persiapan Sebelum Deployment

--- Smart_City/server_smartcity/smartcity_app/asgi.py
+++ Smart_City/server_smartcity/smartcity_app/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iet_2026_24782097.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartcity_app.settings')
 
 application = get_asgi_application()
