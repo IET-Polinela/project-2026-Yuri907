9ef3a3b6e83356a438970aa639ad560f299119a4
Yuri Elva <eyuri6359@gmail.com>
2026-06-15 22:38:33 +0700

    fiks kode api

--- Smart_City/smartcity_citizen_spa_24782097/js/api.js
+++ Smart_City/smartcity_citizen_spa_24782097/js/api.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://103.151.63.88:8013/";
-
+const API_BASE_URL = "http://103.151.63.88:8013";
 
 async function requestAPI(endpoint, method = "GET", bodyData = null) {
     const accessToken = localStorage.getItem("access_token");
